ef488c8c6990269664eafb19578af6864fbb2421
Muhammad Hasnain <mhasnaindev@gmail.com>
2026-07-21 06:30:46 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Sandbox App</h1>
+  <div>
+    <button @click="count++">Clicked {{ count }} Amount of Times!</button>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+
+  div {
+    text-align: center;
+  }
+</style>
